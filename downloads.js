cdc35057b0ba5cbad0f18c684d8e4820c5710606
hstppls <148688549+hstppls@users.noreply.github.com>
2023-12-17 15:23:54 +0100

    Add files via upload

---
+++ downloads.js
@@ -0,0 +1,15 @@
+const download = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.animate');
+hiddenElements.forEach((el) => download.observe(el));
